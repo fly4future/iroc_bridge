4f0d4391e4be069d63be48e3402399e6db59897c
Marlon Rivera <Marlonriv90@gmail.com>
2024-08-19 16:26:16 +0200

    Added into json msg the ETA time from mission feedback

--- src/iroc_bridge.cpp
+++ src/iroc_bridge.cpp
@@ -371,16 +371,20 @@ void IROCBridge::waypointMissionDoneCallback(const SimpleClientGoalState& state,
 
 void IROCBridge::waypointMissionFeedbackCallback(const mrs_mission_manager::waypointMissionFeedbackConstPtr& feedback, const std::string& robot_name) {
   ROS_INFO_STREAM("[IROCBridge]: Feedback from " << robot_name << " action: \"" << feedback->message << "\"" << " goal_idx: " << feedback->goal_idx
-                                                 << " distance_to_closest_goal: " << feedback->distance_to_closest_goal << " goal_progress: " << feedback->goal_progress
-                                                 << " distance_to_finish: " << feedback->distance_to_finish << " mission_progress: " << feedback->mission_progress);
+                                                 << " distance_to_closest_goal: " << feedback->distance_to_closest_goal << " goal_estimated_arrival_time: "
+                                                 << feedback->goal_estimated_arrival_time << " goal_progress: " << feedback->goal_progress
+                                                 << " distance_to_finish: " << feedback->distance_to_finish << " finish_estimated_arrival_time: " 
+                                                 << feedback->finish_estimated_arrival_time  << " mission_progress: " << feedback->mission_progress);
   
   const json json_msg = {
       {"robot_name", robot_name},
       {"mission_state", feedback->message},
       {"current_goal", feedback->goal_idx},
       {"distance_to_goal", feedback->distance_to_closest_goal},
+      {"goal_estimated_arrival_time", feedback->goal_estimated_arrival_time},
       {"goal_progress", feedback->goal_progress},
       {"distance_to_finish", feedback->distance_to_finish},
+      {"finish_estimated_arrival_time", feedback->finish_estimated_arrival_time},
       {"mission_progress", feedback->mission_progress},
   };
   sendJsonMessage("WaypointMissionFeedback", json_msg);
